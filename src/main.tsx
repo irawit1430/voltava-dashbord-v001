a97e389d80a40d953c0fb2c69815d445c4cce6ea
Your Name <your@email.com>
2026-06-27 03:45:57 +0530

    Real cache bust

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 
+console.log("Dashboard version 2.1 - Cache Bust");
+
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
